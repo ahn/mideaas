e543c3df46b229725c3c88f64022542c2091cf47
Antti Nieminen <anttihn@gmail.com>
2014-12-03 19:17:08 +0200

    renaming

--- editortest/src/main/java/org/vaadin/mideaas/ide/BuildComponent.java
+++
@@ -1,9 +0,0 @@
-package org.vaadin.mideaas.ide;
-
-import com.vaadin.ui.Component;
-
-public interface BuildComponent extends Component {
-	
-	
-	
-}
--- mideaas-ide/src/main/java/org/vaadin/mideaas/ide/Util.java
+++ mideaas-ide/src/main/java/org/vaadin/mideaas/ide/Util.java
@@ -1,21 +1,108 @@
 package org.vaadin.mideaas.ide;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.UUID;
+
+import org.kohsuke.github.GHGist;
+import org.kohsuke.github.GHGistFile;
+import org.vaadin.aceeditor.AceMode;
+import org.vaadin.aceeditor.client.AceDoc;
 import org.vaadin.mideaas.editor.AsyncErrorChecker;
-import org.vaadin.mideaas.editor.XmlAsyncErrorChecker;
-import org.vaadin.mideaas.ide.java.JavaErrorChecker;
-import org.vaadin.mideaas.ide.model.SharedProject;
+import org.vaadin.mideaas.editor.DocDiffMediator.Filter;
+import org.vaadin.mideaas.editor.DocDiffMediator.Guard;
+import org.vaadin.mideaas.editor.MultiUserDoc;
 
 public class Util {
+
 	
-	public static AsyncErrorChecker checkerForName(SharedProject project, String name) {
-		if (name.endsWith(".java")) {
-			String cls = project.getPackageName()+"."+name.substring(0, name.length()-5);
-			return new JavaErrorChecker(cls, project.getCompiler());
+	public static IdeProject projectFromGist(GHGist gist, ProjectCustomizer cust) {
+		
+		String name = UUID.randomUUID().toString();
+		
+		Map<String, String> files = new TreeMap<String, String>();
+		for (Entry<String, GHGistFile> e : gist.getFiles().entrySet()) {
+			files.put(e.getKey(), e.getValue().getContent());
 		}
-		else if (name.endsWith(".xml")) {
-			return new XmlAsyncErrorChecker();
+		
+//		return createProject(files, cust);
+		
+		IdeProject project = cust.createProject(name, files);
+		
+		for (Entry<String, GHGistFile> e : gist.getFiles().entrySet()) {
+			IdeDoc ideDoc = docFromGistFile(e.getValue(), cust);
+			project.putDoc(e.getKey(), ideDoc);
 		}
-		return null;
+		
+		return project;
 	}
+
+	private static IdeDoc docFromGistFile(GHGistFile file, ProjectCustomizer cust) {
+		MultiUserDoc mud = customizedDoc(file.getFileName(), file.getContent(), cust);
+		return new IdeDoc(mud, aceModeForGistFile(file));
+	}
+
+	private static AceMode aceModeForGistFile(GHGistFile file) {
+		String lang = file.getLanguage();
+		if (lang==null) {
+			return AceMode.text;
+		}
+		try {
+			return AceMode.valueOf(aceLangFromGithubLang(lang));
+		}
+		catch (IllegalArgumentException e) {
+			return AceMode.text;
+		}
+	}
+
+	private static String aceLangFromGithubLang(String lang) {
+		System.out.println("lang " + lang);
+		return lang.toLowerCase();
+	}
+	
+	private static String INDEX_HTML = "<!DOCTYPE html>\n<html>\n    <head>\n        <title>Hello</title>\n"
+			+ "        <link href=\"./style.css\" rel=\"stylesheet\" />\n    <head>\n"
+			+ "    <body>\n        <p>Hello!</p>\n    </body>\n</html>\n";
+	
+	private static String STYLE_CSS = "body {\n    color: red;\n}\n";
+
+	private static MultiUserDoc customizedDoc(String filename, String content, ProjectCustomizer cust) {
+		Guard upGuard = cust.getUpwardsGuardFor(filename);
+		Guard downGuard = cust.getDownwardsGuardFor(filename);
+		Filter filter = cust.getFilterFor(filename);
+		AsyncErrorChecker checker = cust.getErrorCheckerFor(filename);
+		return new MultiUserDoc(new AceDoc(content), filter, upGuard, downGuard, checker);
+	}
+	
+	public static IdeProject createDemoProject(ProjectCustomizer cust) {
+		String name = UUID.randomUUID().toString();
+		
+		Map<String,String> noFiles = Collections.emptyMap();
+		IdeProject project = cust.createProject(name, noFiles);
+		
+		MultiUserDoc doc1 = customizedDoc("index.html", INDEX_HTML, cust);
+		project.putDoc("index.html", new IdeDoc(doc1, AceMode.html));
+
+		MultiUserDoc doc2 = customizedDoc("style.css", STYLE_CSS, cust);
+		project.putDoc("style.css", new IdeDoc(doc2, AceMode.css));
+		
+		return project;
+	}
+
+	public static IdeProject createProject(Map<String, String> contents, ProjectCustomizer cust) {
+		IdeProject project = cust.createProject(UUID.randomUUID().toString(), contents);
+		for (Entry<String, String> e : contents.entrySet()) {
+			MultiUserDoc doc = customizedDoc(e.getKey(), e.getValue(), cust);
+			project.putDoc(e.getKey(), new IdeDoc(doc, aceModeForFilename(e.getKey())));
+		}
+		return project;
+	}
+
+	private static AceMode aceModeForFilename(String filename) {
+		return AceMode.forFile(filename);
+	}
+	
 
 }
