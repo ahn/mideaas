1a32ab7bad1ee91b5b692766ac1d727c95cc3493
Antti Nieminen <anttihn@gmail.com>
2014-12-20 14:18:28 +0200

    SimpleView fix

--- mideaas-ide/src/main/java/org/vaadin/mideaas/ide/SimpleView.java
+++ mideaas-ide/src/main/java/org/vaadin/mideaas/ide/SimpleView.java
@@ -1,13 +1,10 @@
 package org.vaadin.mideaas.ide;
 
 import org.vaadin.aceeditor.Suggester;
-import org.vaadin.mideaas.editor.EditorUser;
-import org.vaadin.mideaas.editor.MultiUserEditor;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.VerticalSplitPanel;
 
 @SuppressWarnings("serial")
 public class SimpleView extends CustomComponent implements View {
@@ -18,6 +15,7 @@ public class SimpleView extends CustomComponent implements View {
 		this.title = title;
 		IdeEditorComponent ed = new IdeEditorComponent(doc, user, suggester);
 		ed.setSizeFull();
+		setSizeFull();
 		setCompositionRoot(ed);
 	}
 	
